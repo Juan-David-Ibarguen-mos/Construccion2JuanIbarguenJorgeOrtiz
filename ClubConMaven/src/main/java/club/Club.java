4b7eae9337072cb5f4f9a6606fa68282b6585508
JAOM361 <jorgeandresortizmena8@gmail.com>
2024-08-28 17:21:01 -0500

    modify ant to maven

--- ClubConMaven/src/main/java/club/Club.java
+++ ClubConMaven/src/main/java/club/Club.java
@@ -15,7 +15,8 @@ public class Club {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola");
+// TODO code application logic here
     }
     
 }
